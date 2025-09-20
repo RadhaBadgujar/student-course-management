e1884100b8459540d53eba659678e1e2b500b8a7
HarshilVora8160 <harshilvora8160@gmail.com>
2025-09-20 11:47:39 +0530

    Day 1: Project skeleton with dependencies

---
+++ src/main/java/com/example/stmc/StmcApplication.java
@@ -0,0 +1,14 @@
+package com.example.stmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StmcApplication.class, args);
+        System.out.println("hello");
+	}
+
+}
